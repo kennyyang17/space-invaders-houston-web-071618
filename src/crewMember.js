4a5e000b05b99a342d67cb9912dfbb9854796d9d
Kenny Yang <kennyyang19@gmail.com>
2018-08-31 15:49:42 -0500

    Done.

--- src/crewMember.js
+++ src/crewMember.js
@@ -1,1 +1,34 @@
-class CrewMember {}
+class CrewMember {
+  constructor(position) {
+    this.position = position
+    this.currentShip = "Looking for a Rig"
+  }
+
+  engageWarpDrive() {
+    if (this.position !== "Pilot" || this.currentShip === "Looking for a Rig") {
+      return "had no effect"
+    } else {
+      this.currentShip.warpDrive = "engaged"
+    }
+  }
+
+  setsInvisibility() {
+    if (this.position !== "Defender" || this.currentShip === "Looking for a Rig") {
+      return "had no effect"
+    } else {
+      this.currentShip.cloaked = true
+    }
+
+  }
+
+  chargePhasers() {
+    if (this.position !== "Gunner" || this.currentShip === "Looking for a Rig") {
+      return "had no effect"
+    } else {
+      this.currentShip.phasersCharge = "charged"
+    }
+
+  }
+}
+
+ryhan = new CrewMember()
